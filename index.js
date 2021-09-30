a8a1ff5ca748e50fc240b915d6227b767ac0ed2d
alguna <o.roche@alguna.fr>
2021-09-30 10:36:07 +0200

    projet setup

---
+++ index.js
@@ -0,0 +1,34 @@
+// Import packages
+const Bundler = require("parcel-bundler");
+const express = require("express");
+const socketIO = require("socket.io");
+const path = require("path");
+
+// Configuration
+const PORT = process.env.PORT || 3000;
+let bundler = new Bundler(path.join(__dirname, "index.html"));
+
+// Start server
+const server = express()
+  .use(bundler.middleware())
+  .listen(PORT, () => console.log("Listening on localhost:" + PORT));
+
+// Initiatlize SocketIO
+// const io = socketIO(server);
+
+// Register "connection" events to the WebSocket
+// io.on("connection", function (socket) {
+//   console.log("io connecté");
+
+//   // Register "join" events, requested by a connected client
+//   socket.on("join", function (room) {
+//     console.log("client connecté");
+//     // join channel provided by client
+//     socket.join(room);
+//     // Register "image" events, sent by the client
+//     socket.on("image", function (msg) {
+//       // Broadcast the "image" event to all other clients in the room
+//       socket.broadcast.to(room).emit("image", msg);
+//     });
+//   });
+// });
